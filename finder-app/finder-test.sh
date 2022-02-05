84e6461cfd884f532a767be84bcf893f1a2065fe
ruchitnaik <ruchit.naik@colorado.edu>
2022-02-05 02:46:53 -0700

    bug fixed: syntex error

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -19,14 +19,14 @@ fi
 WRITER_BIN="writer"
 #find path for writer binary
 path=$(which writer)
-if [ -z $(path) ]; then
+if [ -z $path ]; then
 	WRITER_BIN="./writer"
 fi
 
 FINDER_BIN="finder.sh"
 #find path for finder.sh script
 path=$(which finder.sh)
-if [ -z $(path) ]; then
+if [ -z $path ]; then
 	FINDER_BIN="./finder.sh"
 fi
 
